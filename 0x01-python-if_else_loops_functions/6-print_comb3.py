59ed85d7ea053ada7629389339495339cac43ad5
Rajaa ElMor <rajaa.elmorabet@gmail.com>
2024-03-12 18:20:34 -0700

    prints numbers from 0 to 99 in 2 digits different

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,4 +2,5 @@
 for i in range (0, 10):
     for j in range(i + 1, 10):
         if i < j:
-            print("{:d}{:d}".format(i, j), end="\n" if i == 8 and j == 9 else ", ")
+            print("{:d}{:d}".format(i, j),
+                end="\n" if i == 8 and j == 9 else ", ")
