a3fa178d343ae0afb1b81bc948288687fd1b8511
Rajaa ElMor <rajaa.elmorabet@gmail.com>
2024-03-12 18:47:08 -0700

    is upper

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def uppercase(s):
+    result = ""
+    for char in s:
+        if ord(char) >= 97 and ord(char) <= 122:
+            result += chr(ord(char) - 32)
+        else:
+             result += char
+    print("{}".format(result))
