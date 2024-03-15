ec1fb01cd84262b397a0e8e272f1d44a740ab3c0
Rajaa ElMor <rajaa.elmorabet@gmail.com>
2024-03-14 19:49:41 -0700

    script dynamic

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,14 +4,11 @@ if __name__ == "__main__":
 
     num_args = len(sys.argv) - 1  # to exclude the script name
     if num_args == 0:
-        print("0 argument")
+        print("0 arguments.")
     elif num_args == 1:
         print("1 argument:")
     else:
-        print("{} argument".format(num_args), end='')
-    
-    print("".format('' if num_args == 1 else 's'), end='')
-    print("".format( ':' if num_args else '.'))
+        print("{} arguments".format(num_args))
 
     if num_args > 0:
         for i, arg in enumerate(sys.argv[1:], start=1):
