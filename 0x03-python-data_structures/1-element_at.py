63f66db2c0e7fe13d658174a6568faed5365166e
Rajaa ElMor <rajaa.elmorabet@gmail.com>
2024-03-18 15:37:58 -0700

    element of list

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-def element_at(my_list, index):
-    if index < 0 or  index >= len(my_list):
+
+def element_at(my_list, idx):
+    if idx < 0 or  idx >= len(my_list):
         return None
-    return my_list[index]
+    return my_list[idx]
