b3dd8d0b96bfdcf04badb1d04ec4fe89c9f00ba5
Rajaa ElMor <rajaa.elmorabet@gmail.com>
2024-03-20 15:09:19 -0700

    hidden elements

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    """Print all directories  hidden_4 module."""
+    import hidden_4
+
+    names = dir(hidden_4)
+    for i in names:
+        if i[:2] != "__":
+            print(i)
