5b8a99e80476e9a7e02317c745bf4fee21613134
Rajaa ElMor <rajaa.elmorabet@gmail.com>
2024-03-20 14:05:09 -0700

    add infinint numbers

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-        import sys
-        total_sum = 0
-        for i in range (len(sys.argv) - 1):
-            total_sum = total_sum +  int(sys.argv[i +1])
-        print(total_sum)
+    import sys
+    total_sum = 0
+    for i in range (len(sys.argv) - 1):
+        total_sum = total_sum +  int(sys.argv[i +1])
+    print(total_sum)
