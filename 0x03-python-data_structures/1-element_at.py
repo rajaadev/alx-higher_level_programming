7c67dd844f63cdef47892b10bf88f18a82109983
Rajaa ElMor <rajaa.elmorabet@gmail.com>
2024-03-19 17:18:29 -0700

    print reserved list

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def element_at(my_list, idx):
-    if idx < 0 or  idx >= len(my_list):
-        return (None)
+    if idx not in range(len(my_list)):
+        return None
     else:
-        return (my_list[idx])
+        return my_list[idx]
