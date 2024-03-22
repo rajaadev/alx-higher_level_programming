6adc886f98fe61dfa82b7233fe1f9be4e8ce57de
Rajaa ElMor <rajaa.elmorabet@gmail.com>
2024-03-22 08:17:53 -0700

    square matrix

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
 def square_matrix_map(matrix=[]):
-    return list(map(lambda ligne: list(map(lambda col: col ** 2, ligne)), matrix))
+    return list(map(lambda ligne: list(map(lambda col: col:**2, ligne)), matrix))
