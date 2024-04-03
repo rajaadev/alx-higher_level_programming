abab31e2aa2aa196f326487326271ca419fedf07
rajaa <rajaa.elmorbet@gmail.com>
2024-04-03 10:40:04 -0700

    update

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,8 +5,8 @@ def safe_print_integer_err(value):
     is_integer = True
     try:
         print("{:d}".format(value))
-    except (TypeError, ValueError):
         return (is_integer)
+    except (TypeError, ValueError):
         print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
         is_integer = False
         return (is_integer)
