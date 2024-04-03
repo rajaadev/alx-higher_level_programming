825012807c1fc18fcff805955de666dff922a3a8
rajaa <rajaa.elmorbet@gmail.com>
2024-04-03 07:17:33 -0700

    update

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -4,7 +4,7 @@ def safe_print_list_integers(my_list=[], x=0):
     counter = 0
     try:
         while counter is not x:
-            print("{:d}".format(my_list[counter]), end="")
+            print(my_list[counter], end="")
             counter = counter + 1
     except IndexError:
         None
