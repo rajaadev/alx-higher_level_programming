c76efec345b0fdc05a13d3ce656ab8dd3b6b9e61
rajaa <rajaa.elmorbet@gmail.com>
2024-04-03 18:30:39 -0700

    adding file

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+#define Square class
+
+class Square:
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+#square class
+
+class Square:
+    def __init__(self, size):
+        # Private instance attribute
+        self.__size = size
