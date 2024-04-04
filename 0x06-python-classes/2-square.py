2b0c8afef6ae7d54dec128a305f7bc395ca08027
rajaa <rajaa.elmorbet@gmail.com>
2024-04-03 19:52:46 -0700

    adding file

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
-""" MOdule : Square."""
+""" Module Square"""
 
 
 class Square:
-    """class square."""
+    """Class square"""
 
     def __init__(self, size=0):
         """Initialize a new Square.
