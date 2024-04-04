71dbc7d8f90dde6ff7eaf692e6656361342a8a94
rajaa <rajaa.elmorbet@gmail.com>
2024-04-03 18:50:20 -0700

    adding file

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -5,12 +5,15 @@
 
 
 class Square:
+
     """define classe Square"""
 
     def __init__(self, size):
+
         """constructor new instance.
 
         Args:
             size: lenght of new square.
         """
+
         self.__size = size
