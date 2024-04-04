d34fd96b365fbabd54cbb3f78f2e237c7bfd3f48
rajaa <rajaa.elmorbet@gmail.com>
2024-04-03 19:07:40 -0700

    adding file

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+"""Module Square."""
+
+
+class Square:
+    """class square."""
+
+    def __init__(self, size=0):
+        """Initialize a new square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """size of the square."""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Return the current area of square."""
+        return (self.__size ** 2)
