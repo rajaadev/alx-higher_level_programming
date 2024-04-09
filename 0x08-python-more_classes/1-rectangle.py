5ebc773b3f5a855203c63da36823f33c8c7595a6
rajaa <rajaa.elmorbet@gmail.com>
2024-04-08 18:51:07 -0700

    add file creation of rectangle

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""
+Define Rectangle class
+"""
+
+
+class Rectangle:
+    """ Rectangle class"""
+
+    def __init__(self, width=0, height=0):
+        """
+        Initializes a Rectangle instance.
+        Args:
+            width (int): The width of the rectangle. Default is 0.
+            height (int): The height of the rectangle. Default is 0.
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Getter for the instance of width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setter for the instance of width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Getter for instance  height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setter for instance height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
