9f54a8e31ff110dee4d3f67545ed3d4f3826455f
Rajaa <rajaa.elmorabet@gmail.com>
2024-04-10 06:39:08 +0000

    update

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -14,40 +14,40 @@ class Rectangle:
     @property
     def width(self):
         """Getter for instance width"""
-        return self.__width
+        return (self.__width)
 
     @width.setter
     def width(self, value):
         """Setter for instance width"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        elif value < 0:
+	if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
     @property
     def height(self):
         """Getter for instance height"""
-        return self.__height
+        return (self.__height)
 
     @height.setter
     def height(self, value):
         """Setter for instance height"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
 
     def area(self):
         """Calculates and returns the area rectangle"""
-        return self.__width * self.__height
+        return (self.__width * self.__height)
 
     def perimeter(self):
         """Calculates and returns the perimeter rectangle"""
         if self.__width == 0 or self.__height == 0:
-            return 0
-        return 2 * (self.__width + self.__height)
+            return (0)
+        return (2 * (self.__width + self.__height))
 
 
     def __str__(self):
@@ -56,9 +56,9 @@ class Rectangle:
         rectangle using '#' characters
         """
         if self.__width == 0 or self.__height == 0:
-            return ""
+            return ("")
 
         rectangle_str = ""
         for _ in range(self.__height):
             rectangle_str += "#" * self.__width + "\n"
-        return rectangle_str[:-1]
+        return (rectangle_str[:-1])
