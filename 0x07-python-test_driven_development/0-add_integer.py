3ea5eb9c4fb76ceddc9e75f5469bf08ba780c750
Rajaa <rajaa.elmorabet@gmail.com>
2024-04-11 17:30:19 +0000

    add integer

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,11 +1,12 @@
+
 #!/usr/bin/python3
 """Define two integers or float addition function."""
 
 
 def add_integer(a, b=98):
-	""" add  a and b """
+	""" add twi integers or floats a and b """
 	if type (a) and not in(int, float):
 		raise TypeError("a must be an integer ")
 	if type (b) and not in(int, float): 
 		raise TypeError("b must be an integer ")
-return int(a) + int(b)
+return (int(a) + int(b))
