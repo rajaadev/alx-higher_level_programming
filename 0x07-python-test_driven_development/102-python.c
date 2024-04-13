6968b615d977f0589787e1d10d42e2ba9efd68b9
Rajaa <rajaa.elmorabet@gmail.com>
2024-04-13 03:04:28 +0000

    update

---
+++ 0x07-python-test_driven_development/102-python.c
@@ -0,0 +1,26 @@
+nclude <stdio.h>
+#include <stdlib.h>
+#include <wchar.h>
+#include <locale.h>
+#include <Python.h>
+#include <unicodeobject.h>
+
+/**
+ * print_python_string - prints info about python strings
+ * @p: address of pyobject struct
+ */
+void print_python_string(PyObject *p)
+{
+    wprintf(L"[.] string object info\n");
+    if (!PyUnicode_Check(p))
+    {
+        wprintf(L"  [ERROR] Invalid String Object\n");
+        return;
+    }
+    if (PyUnicode_IS_COMPACT_ASCII(p))
+        wprintf(L"  type: compact ascii\n");
+    else
+        wprintf(L"  type: compact unicode object\n");
+    wprintf(L"  length: %lu\n", PyUnicode_GET_LENGTH(p));
+    wprintf(L"  value: %ls\n", PyUnicode_AS_UNICODE(p));
+}
