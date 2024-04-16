b51620821afb57bd21b1154f2aa01af84b5a1f99
Rajaa <rajaa.elmorabet@gmail.com>
2024-04-16 02:25:30 +0000

    is instance

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -3,5 +3,5 @@
 
 
 def inherits_from(obj, a_class):
-    '''Determines if object is a true subclass of a class.'''
+    '''Determines if object is a subclass of a class.'''
     return isinstance(obj, a_class) and type(obj) != a_class
