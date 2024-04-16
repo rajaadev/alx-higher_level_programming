98c0146fcb544593ac69293307a993c0bfcd11fc
Rajaa <rajaa.elmorabet@gmail.com>
2024-04-16 00:01:56 +0000

    lookup function

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+''' Module for lookup method'''
+
+def lookup(obj):
+    '''Looks up for objects attributes and methods.
+    Args:
+	obj (object): the object to list.
+
+    Return:
+        list: the list of attributes and methodes.
+    '''
+    return dir(obj)
