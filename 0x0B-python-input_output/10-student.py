4f3dc3827b229a14fc886313c82dcc9a7b427b8c
Rajaa <rajaa.elmorabet@gmail.com>
2024-04-17 01:52:14 +0000

    add files

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+class Student:
+    '''Defines a student.'''
+
+    def __init__(self, first_name, last_name, age):
+        '''Initializes a student with first_name, last_name, and age.'''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        '''Retrieves a dictionary representation of a Student instance.'''
+        if attrs is None:
+            return self.__dict__
+        else:
+            return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
