375213daf3ae26eab3aef1b0561dc30637931f3b
Rajaa <rajaa.elmorabet@gmail.com>
2024-04-17 02:07:31 +0000

    add files

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 import sys
 
 def print_metrics(total_size, status_codes):
--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -12,7 +12,8 @@ def to_json_string(my_obj):
         items = ', '.join(to_json_string(item) for item in my_obj)
         return '[' + items + ']'
     elif isinstance(my_obj, dict):
-        pairs = ', '.join(f'"{key}": {to_json_string(value)}' for key, value in my_obj.items())
+        pairs = ', '.join(f'"{key}": {to_json_string(value)}'\
+		for key, value in my_obj.items())
         return '{' + pairs + '}'
     else:
         raise TypeError(f"{my_obj} is not JSON serializable")
