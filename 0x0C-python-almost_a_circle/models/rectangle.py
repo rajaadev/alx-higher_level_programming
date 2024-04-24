ff76c83971279e3b080b40cb0b369fa7903dc96d
Rajaa <rajaa.elmorabet@gmail.com>
2024-04-24 15:56:30 +0000

    update

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-'''Module Rectangle class.'''
+'''Module for Rectangle class.'''
 from models.base import Base
 
 
 class Rectangle(Base):
-    '''Rectangle class.'''
+    '''A Rectangle class.'''
 
     def __init__(self, width, height, x=0, y=0, id=None):
         '''Constructor.'''
@@ -16,38 +16,49 @@ class Rectangle(Base):
 
     @property
     def width(self):
-        '''Width rectangle.'''
+        '''Width of this rectangle.'''
         return self.__width
 
     @width.setter
     def width(self, value):
+        self.validate_integer("width", value)
         self.__width = value
 
     @property
     def height(self):
-        '''Height rectangle.'''
+        '''Height of this rectangle.'''
         return self.__height
 
     @height.setter
     def height(self, value):
+        self.validate_integer("height", value)
         self.__height = value
 
     @property
     def x(self):
-        '''x rectangle.'''
+        '''x of this rectangle.'''
         return self.__x
 
     @x.setter
     def x(self, value):
+        self.validate_integer("x", value, positive=False)
         self.__x = value
 
     @property
     def y(self):
-        '''y rectangle.'''
+        '''y of this rectangle.'''
         return self.__y
 
     @y.setter
     def y(self, value):
+        self.validate_integer("y", value, positive=False)
         self.__y = value
-
-
+    
+    def validate_integer(self, name, value, positive=True):
+        '''Method for validating the value.'''
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if positive and value <= 0:
+            raise ValueError("{} must be > 0".format(name))
+        elif not positive and value < 0:
+            raise ValueError("{} must be >= 0".format(name))
