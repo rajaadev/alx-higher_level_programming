780607f94683d81f13cc5f9771ddc497047fa79d
Rajaa <rajaa.elmorabet@gmail.com>
2024-04-27 00:40:46 +0000

    update

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -17,36 +17,41 @@ class Rectangle(Base):
 
     @property
     def width(self):
-        '''Width rectangle.'''
+        '''Get the width of the rectangle.'''
         return self.__width
 
     @width.setter
     def width(self, value):
+        '''Set the width of the rectangle.'''
         self.__width = value
 
     @property
     def height(self):
-        '''Height rectangle.'''
+        '''Get the height of the rectangle.'''
         return self.__height
 
     @height.setter
     def height(self, value):
+        '''Set the height of the rectangle.'''
         self.__height = value
 
     @property
     def x(self):
-        '''x rectangle.'''
+        '''Get the x-coordinate of the rectangle.'''
         return self.__x
 
     @x.setter
     def x(self, value):
+        '''Set the x-coordinate of the rectangle.'''
         self.__x = value
 
     @property
     def y(self):
-        '''y rectangle.'''
+        '''Get the y-coordinate of the rectangle.'''
         return self.__y
 
     @y.setter
     def y(self, value):
+        '''Set the y-coordinate of the rectangle.'''
         self.__y = value
+
