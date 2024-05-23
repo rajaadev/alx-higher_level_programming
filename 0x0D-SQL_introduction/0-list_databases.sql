c0cc210e78db93e1748f331ff38a1d6a705369e2
Rajaa <rajaa.elmorabet@gmail.com>
2024-05-23 20:42:07 +0000

    list database

--- 0x0D-SQL_introduction/0-list_databases.sql
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -1,10 +1,1 @@
-#!/bin/bash
-
-# Create a temporary SQL script file
-echo "SHOW DATABASES;" > /tmp/temp_list_databases.sql
-
-# Execute the SQL script using the MySQL client
-mysql -hlocalhost -uroot -p < /tmp/temp_list_databases.sql
-
-# Remove the temporary SQL script file
-rm /tmp/temp_list_databases.sql
+SHOW DATABASES;
