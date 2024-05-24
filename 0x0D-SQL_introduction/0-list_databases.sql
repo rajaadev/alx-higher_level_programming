217c17af89efdac99da1b37816740198c2cdd16e
Rajaa <rajaa.elmorabet@gmail.com>
2024-05-24 02:28:50 +0000

    list database

--- 0x0D-SQL_introduction/0-list_databases.sql
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -1,5 +1,4 @@
---script that lists all databases of your MySQL server
-
+-- script that lists all databases of MySQL server
 -- Script to creat Database  hbtn_0c_0 if it does not exist
 CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
 -- Script to list all databases
