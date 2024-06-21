3abf86316f35d3e06d64eaa8873633025d3d37d0
= <rajaa.elmorabet@gmail.com>
2024-06-21 12:40:46 -0700

    2-rectangle

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -3,6 +3,9 @@
 // class Rectangle
 class Rectangle {
   constructor(w, h) {
+    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
+      return {}; // Returning an empty object if w or h is not a positive integer
+    }
     this.width = w;
     this.height = h;
   }
