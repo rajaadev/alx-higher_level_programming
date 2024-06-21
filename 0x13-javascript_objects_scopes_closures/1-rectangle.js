8e5d092c55d684dad61461494bb627d05064014c
= <rajaa.elmorabet@gmail.com>
2024-06-21 12:54:07 -0700

    1-rectangle

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,14 +1,10 @@
-#!/usr/bin/node
-
 // class Rectangle
 class Rectangle {
-  constructor(w, h) {
-    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
-      return {}; // Returning an empty object if w or h is not a positive integer
-    }
-    this.width = w;
-    this.height = h;
-  }
+constructor(w, h) {
+this.width = w;
+this.height = h;
+}
 }
 
 module.exports = Rectangle;
+
