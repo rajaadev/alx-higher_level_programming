82b03a6d62e68072ae96ba7c33800dd380fe3124
= <rajaa.elmorabet@gmail.com>
2024-06-22 02:50:22 -0700

    0-select_states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,6 +7,7 @@ import sys
 import MySQLdb
 
 if __name__ == "__main__":
+    # Get MySQL credentials and database name from command line arguments
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
