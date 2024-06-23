3fb9b9a43bc689bf2838af9a706ab8a04877fd49
= <rajaa.elmorabet@gmail.com>
2024-06-22 17:04:41 -0700

    2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -10,35 +10,14 @@ import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    # Get MySQL credentials and database name from command line arguments
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    state_name = sys.argv[4]
+db_connect = db.connect(host="localhost", port=3306,
+                            user=argv[1], passwd=argv[2], db=argv[3])
+    db_cursor = db_connect.cursor()
 
-    # Connect to MySQL database
-    db = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=username,
-            passwd=password,
-            db=database)
+    db_cursor.execute(
+        "SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY \
+                        states.id ASC".format(argv[4]))
+    rows_selected = db_cursor.fetchall()
 
-    # Create a cursor to execute queries
-    cursor = db.cursor()
-
-    # Execute the query to get states matching the argument
-    query = "SELECT * FROM states WHERE name = '{}' ORDER BY \
-            states.id ASC".format(state_name)
-    cursor.execute(query)
-
-    # Fetch all the rows
-    rows = cursor.fetchall()
-
-    # Print each row
-    for row in rows:
+    for row in rows_selected:
         print(row)
-
-    # Close the cursor and database connection
-    cursor.close()
-    db.close()
