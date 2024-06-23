1d141619a6f5cd5b644d0822b4cd931bdac227b6
= <rajaa.elmorabet@gmail.com>
2024-06-22 17:07:25 -0700

    2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
 """
-A script that takes in an argument and 
+This script takes in an argument and
 displays all values in the states
-table of hbtn_0e_0_usa 
-where name matches the argument.
+where `name` matches the argument
+from the database `hbtn_0e_0_usa`.
 """
 
 import MySQLdb as db
 from sys import argv
 
-if __name__ == "__main__":
-db_connect = db.connect(host="localhost", port=3306,
+if __name__ == '__main__':
+    """
+    Access to the database and get the states
+    from the database.
+    """
+    db_connect = db.connect(host="localhost", port=3306,
                             user=argv[1], passwd=argv[2], db=argv[3])
     db_cursor = db_connect.cursor()
 
