c3f65a8f2a510c0c73460466e7e28a2acc8f39c0
= <rajaa.elmorabet@gmail.com>
2024-06-22 17:29:47 -0700

    4-cities_by_state.py

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -5,37 +5,24 @@ The script takes 3 arguments: mysql username, mysql password, and database name.
 The results are sorted in ascending order by cities.id.
 """
 
-import sys
-import MySQLdb
+import MySQLdb as db
+from sys import argv
 
-if __name__ == "__main__":
-    # Get MySQL credentials and database name from command line arguments
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
-    # Connect to MySQL database
-    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
-
-    # Create a cursor to execute queries
-    cursor = db.cursor()
-
-    # Execute the query to get all cities with their corresponding states
-    query = """
-    SELECT cities.id, cities.name, states.name
-    FROM cities
-    JOIN states ON cities.state_id = states.id
-    ORDER BY cities.id ASC
+if __name__ == '__main__':
     """
-    cursor.execute(query)
+    Access the database and get the cities
+    from the database.
+    """
 
-    # Fetch all the rows
-    rows = cursor.fetchall()
+    db_connect = db.connect(host="localhost", port=3306,
+                            user=argv[1], passwd=argv[2], db=argv[3])
 
-    # Print each row
-    for row in rows:
-        print(row)
+    with db_connect.cursor() as db_cursor:
+        db_cursor.execute("SELECT cities.id, cities.name, states.name \
+                                FROM cities JOIN states ON cities.state_id \
+                                = states.id ORDER BY cities.id ASC")
+        rows_selected = db_cursor.fetchall()
 
-    # Close the cursor and database connection
-    cursor.close()
-    db.close()
+    if rows_selected is not None:
+        for row in rows_selected:
+            print(row)
