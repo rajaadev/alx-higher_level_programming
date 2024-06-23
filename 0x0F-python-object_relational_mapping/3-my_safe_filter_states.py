0d1a230b35eec97face82cd0338e7c9d2267e00f
= <rajaa.elmorabet@gmail.com>
2024-06-22 17:10:08 -0700

    3-my_safe_filter_states.py

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """
-A script that takes in an argument and displays all values in the states
-table of hbtn_0e_0_usa where name matches the argument.
+A script that takes in an argument and displays
+all values in the states table of hbtn_0e_0_usa
+where name matches the argument.
 This script is safe from SQL injection.
 """
 
