df84e419251533b5e042f5e07ba25b93d9ccf89f
= <rajaa.elmorabet@gmail.com>
2024-06-22 17:27:35 -0700

    3-my_safe_filter_states.py

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """
-This script takes in an argument and displays
-all values in the states where `name`matches
-the argument from the database `hbtn_0e_0_usa`.
+This script takes in an argument and
+displays all values in the states
+where `name` matches the argument
+from the database `hbtn_0e_0_usa`.
 This time the script is safe from
 MySQL injections!
 """
