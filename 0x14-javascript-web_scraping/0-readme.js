6b9884515bc681ec4749fcea80d398928ebf9821
= <rajaa.elmorabet@gmail.com>
2024-07-31 14:47:07 -0700

    add readme.js

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+// Get the file path from the command line arguments
+const filePath = process.argv[2];
+
+// Read the file content
+fs.readFile(filePath, 'utf-8', (err, data) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(data);
+  }
+});
---
+++ 0x14-javascript-web_scraping/README.md
@@ -0,0 +1,7 @@
+# File Reader Script
+
+a simple Node.js script that reads the content of a specified file and prints it to the console.
+
+## Requirements
+
+- Node.js
