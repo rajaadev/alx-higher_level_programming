8fcc2e91d45a2c2238e0949493bc45db6232f80a
Rajaa <rajaa.elmorabet@gmail.com>
2024-09-19 11:18:43 +0000

    using jquery

---
+++ 0x15-javascript-web_jquery/1-script.js
@@ -0,0 +1,2 @@
+// Use jQuery to select the <header> element and change its text color to red
+$('header').css('color', '#FF0000');
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-0x13. JavaScript - Objects, Scopes and Closures
+alx-higher_level_programming
