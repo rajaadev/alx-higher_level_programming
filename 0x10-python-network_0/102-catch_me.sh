6984b7fd7b21adc379c528eee17ecc6f44462cf0
rajaa <rajaa.elmorabet@gmail.com>
2025-08-10 20:09:27 -0700

    102-catch_me.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Envoie une requête GET vers /catch_me et affiche le corps de la réponse
-curl -s -X GET -H "User-Agent: curl" http://0.0.0.0:5000/catch_me
+# Envoie une requête POST vers /catch_me et affiche le corps de la réponse
+curl -s -X POST http://0.0.0.0:5000/catch_me
