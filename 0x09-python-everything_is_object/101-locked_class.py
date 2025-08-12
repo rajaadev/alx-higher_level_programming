6fe43800b93d2c6a127a3dd69620c30bc5193831
rajaa <rajaa.elmorabet@gmail.com>
2025-08-11 17:04:03 -0700

    101-locked_class.py

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -3,8 +3,8 @@
 
 class LockedClass:
     """Class that only allows an instance attribute called 'first_name'.
-
     Prevents dynamically creating any other instance attributes.
     """
+
 
     __slots__ = ['first_name']
